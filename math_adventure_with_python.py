0ab4d3edb1400f14781ba4cfda80ec7e29d08875
jusjaesho <jusjaesho@gmail.com>
2020-02-02 18:24:08 -0600

    Chapter 1 Done

--- math_adventure_with_python.py
+++ math_adventure_with_python.py
@@ -1,9 +1,9 @@
 from turtle import *
+import time
 
 # 1-1 Square Dance
 
-shape('turtle')
-speed(5)
+speed(0)
 
 # def square(sidelength = 100):
 # 	for i in range(4):
@@ -25,12 +25,61 @@ speed(5)
 
 # triangle(450)
 
+# 1-4 Polygon
 
-def polygon(sidelength = 100, sides = 6):
-	for side in range(sides):
-		forward(sidelength)
-		right(360 / sides)
+# def polygon(sidelength = 100, sides = 6):
+# 	for side in range(sides):
+# 		forward(sidelength)
+# 		right(360 / sides)
 
-polygon(40, 4)
+# polygon(40, 10)
+
+# 1-5 Turtle Spiral
+
+# def spiralSquare(squares, length, change):
+# 	for i in range(squares):
+# 		for j in range(4):
+# 			forward(length)
+# 			right(90)
+# 		right(change)
+# 		length += 5
+
+# spiralSquare(60, 5, 5)
+
+# 1-6 A Star is Born
+
+# def star(length = 100):
+# 	for i in range(5):
+# 		forward(sidelength)
+# 		right(144)
+
+# star(15)
+
+# def spiralStar(stars, length, change):
+# 	for i in range(stars):
+# 		for j in range(5):
+# 			forward(length)
+# 			right(144)
+# 		right(change)
+# 		length += 5
+
+# spiralStar(60, 5, 5)
+
+
+
+time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
